7c7b4605f40c2f2c4b8f67b24bddbe4eda73cb1d
unknown <samarthkhare17@gmail.com>
2025-04-08 23:18:09 +0100

    commit

---
+++ Query7_TripsCancelled.sql
@@ -0,0 +1,45 @@
+/*
+Create table  Trips (id int, client_id int, driver_id int, city_id int, status varchar(50), request_at varchar(50));
+Create table Users (users_id int, banned varchar(50), role varchar(50));
+Truncate table Trips;
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('1', '1', '10', '1', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('2', '2', '11', '1', 'cancelled_by_driver', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('3', '3', '12', '6', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('4', '4', '13', '6', 'cancelled_by_client', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('5', '1', '10', '1', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('6', '2', '11', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('7', '3', '12', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('8', '2', '12', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('9', '3', '10', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('10', '4', '13', '12', 'cancelled_by_driver', '2013-10-03');
+Truncate table Users;
+insert into Users (users_id, banned, role) values ('1', 'No', 'client');
+insert into Users (users_id, banned, role) values ('2', 'Yes', 'client');
+insert into Users (users_id, banned, role) values ('3', 'No', 'client');
+insert into Users (users_id, banned, role) values ('4', 'No', 'client');
+insert into Users (users_id, banned, role) values ('10', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('11', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('12', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('13', 'No', 'driver');
+*/
+
+/*Write a sql query to find the cancellation rate of requests with banned users (both client and driver must not be banned )
+each day between "2013-10-01" and "2013-10-03". round of to 2 decimal place
+
+cancellation rate - dividing no of cancelled (by client or driver) request with unbanned by total no of request by unbanned user on that 
+day*/
+
+
+select request_at,
+	SUM(CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END) AS Can_count,
+	COUNT(id) AS tot_trip,
+	100*ROUND(1.0*SUM(CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END)/COUNT(id),2) AS can_rate
+	FROM trips t
+	LEFT JOIN Users U ON T.client_id = U.users_id 
+	LEFT JOIN Users D ON T.driver_id = D.users_id
+	WHERE u.banned ='No' and d.banned = 'No'
+	
+	GROUP BY request_at
+
+
+;
